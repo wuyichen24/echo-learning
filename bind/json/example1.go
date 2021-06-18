7138e77372a29d55c06c2cf03fc8520ed7ab096d
Wuyi Chen <wuyichen24@gmail.com>
2021-06-18 15:21:15 -0600

    Initial commit

---
+++ bind/json/example1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type User struct {
+	Name  string `json:"name" form:"name" query:"name"`
+	Email string `json:"email" form:"email" query:"email"`
+}
+
+func main() {
+	e := echo.New()
+	e.POST("/users", func(c echo.Context) (err error) {
+		u := new(User)
+		if err = c.Bind(u); err != nil {
+			return
+		}
+		return c.JSON(http.StatusOK, u)
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
